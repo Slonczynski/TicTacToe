0655118edb21481bf1f2323f543d8e491c6f3487
Konrad <15710231+Olimpus444@users.noreply.github.com>
2019-04-14 11:26:09 +0200

    basic structure

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -10,6 +10,20 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
+            string nicknamePlayerA = "";
+            string nicknamePlayerB = "";
+            char markPlayerA = 'x';
+            char markPlayerB = 'o';
+            char[,] board = new char[3, 3] {
+                { '1', '2', '3' },
+                { '4', '5', '6' },
+                { '7', '8', '9' }
+            };
+
+            Console.Write("Wpisz imie gracza A: ");
+            nicknamePlayerA = Console.ReadLine();
+            Console.Write("Wpisz imie gracza B: ");
+            nicknamePlayerB = Console.ReadLine();
         }
     }
 }
