3ec64748285050fb425473feb9daaba25ef5448b
konrad.slonczynski <konrad.slonczynski@PK2-15>
2019-05-04 15:57:57 +0200

    Add board

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TicTacToe
 {
@@ -20,10 +16,32 @@ namespace TicTacToe
                 { '7', '8', '9' }
             };
 
+            bool endGame = false;
+            while(!endGame)
+            {
+                Console.Clear();
+                DrawBoard(board);
+                Console.ReadKey();
+            }
+
             Console.Write("Wpisz imie gracza A: ");
             nicknamePlayerA = Console.ReadLine();
             Console.Write("Wpisz imie gracza B: ");
             nicknamePlayerB = Console.ReadLine();
+        }
+        static void DrawBoard(char[,] board) 
+        {
+            int height = board.GetLength(0);
+            int width = board.GetLength(1);
+
+            for (int i = 0; i < height; ++i)
+            {
+                for (int j = 0; j < width; ++j)
+                {
+                    Console.Write(board[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
