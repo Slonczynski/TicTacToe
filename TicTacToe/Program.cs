fc2e61786f424275726c84eb8f1b3ec0b5f3e8da
konrad.slonczynski <konrad.slonczynski@PK2-15>
2019-05-18 15:56:04 +0200

    Classes added

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,10 +6,15 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            string nicknamePlayerA = "";
-            string nicknamePlayerB = "";
-            char markPlayerA = 'x';
-            char markPlayerB = 'o';
+            HumanPlayer gA = new HumanPlayer();
+            ComputerPlayer gB = new ComputerPlayer();
+
+            gA.Name = "User";
+            gB.Name = "Computer";
+            gA.Sign = "x";
+            gB.Sign = "o";
+
+       
             char[,] board = new char[3, 3] {
                 { '1', '2', '3' },
                 { '4', '5', '6' },
@@ -17,17 +22,25 @@ namespace TicTacToe
             };
 
             bool endGame = false;
+            bool playerMoveA = ture;
             while(!endGame)
             {
                 Console.Clear();
                 DrawBoard(board);
+                if (playerMoveA)
+                    {
+                Console.WriteLine("Ruch wykonuje: " + gA.Name);
+                endGame = gA.MakeMove(board);
+                playerMoveA = false;
+                    }  
+                else
+                    {
+                Console.WriteLine("Ruch wykonuje: " + gB.Name);
+                    endGame = gB.MakeMove(board);
+                    playerMoveA = true;
+}
                 Console.ReadKey();
             }
-
-            Console.Write("Wpisz imie gracza A: ");
-            nicknamePlayerA = Console.ReadLine();
-            Console.Write("Wpisz imie gracza B: ");
-            nicknamePlayerB = Console.ReadLine();
         }
         static void DrawBoard(char[,] board) 
         {
@@ -44,4 +57,28 @@ namespace TicTacToe
             }
         }
     }
+    interface IMove
+    {
+        bool MakeMove(char[,] board);
+    }
+    abstract class Player
+    {
+        public string Name { get; set; }
+        public char Sign { get; set; }
+    }
+        class HumanPlayer : Player, IMove 
+        {
+        public bool MakeMove(char[,] board)
+        {
+        return false;
+        }
+        }
+
+       class ComputerPlayer : Player, IMove 
+        {
+            public bool MakeMove(char[,] board)
+            {
+            return false;
+            }
+        }
 }
