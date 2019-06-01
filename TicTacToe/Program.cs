7f12b66a9fd1cb59985158404b2f8bc39304cc88
konrad.slonczynski <konrad.slonczynski@PK2-15>
2019-06-01 15:57:27 +0200

    Add new function

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -20,29 +20,30 @@ namespace TicTacToe
                 { '4', '5', '6' },
                 { '7', '8', '9' }
             };
+            char[,] boardCopy = board.Clone() as char[,];
 
             bool endGame = false;
             bool playerMoveA = ture;
-            while(!endGame)
+            for (int round = 0; round < board.Length; ++round)
             {
                 Console.Clear();
                 DrawBoard(board);
-                if (playerMoveA)
+                    if (playerMoveA)
                     {
-                Console.WriteLine("Ruch wykonuje: " + gA.Name);
-                endGame = gA.MakeMove(board);
-                playerMoveA = false;
+                        Console.WriteLine("Ruch wykonuje: " + gA.Name);
+                        endGame = gA.MakeMove(board, boardCopy);
+                        playerMoveA = false;
                     }  
-                else
+                    else
                     {
-                Console.WriteLine("Ruch wykonuje: " + gB.Name);
-                    endGame = gB.MakeMove(board);
-                    playerMoveA = true;
-}
-                Console.ReadKey();
+                        Console.WriteLine("Ruch wykonuje: " + gB.Name);
+                        endGame = gB.MakeMove(board, boardCopy);
+                        playerMoveA = true;
+                    }
+            Console.ReadKey();
             }
         }
-        static void DrawBoard(char[,] board) 
+        static void DrawBoard(char[,] board, char[,] boardCopy) 
         {
             int height = board.GetLength(0);
             int width = board.GetLength(1);
@@ -53,32 +54,84 @@ namespace TicTacToe
                 {
                     Console.Write(board[i, j]);
                 }
-                Console.WriteLine();
+            Console.WriteLine();
             }
         }
     }
     interface IMove
     {
-        bool MakeMove(char[,] board);
+        bool MakeMove(char[,] board, char[,] boardCopy);
     }
     abstract class Player
     {
         public string Name { get; set; }
         public char Sign { get; set; }
-    }
-        class HumanPlayer : Player, IMove 
+
+        public bool CheckIfGameEnded(char[,] board)
         {
-        public bool MakeMove(char[,] board)
+            int height = board.GetLength(0);
+            int width = board.GetLength(1);
+            if (height != width)
+                throw new Exception("Plansza nie jest kwadratowa!");
+
+            // check lines
+
+            for (int i = 0; i < width; i++) 
+            {
+                int sumLine = 0;
+                for (int i = 0; i < width; ++j)
+                {
+                if (board[i,j] == Sign)
+                        ++sumLine;
+                }
+            if (sumLine == width)
+                    return true;
+            }
+        
+            for (int j = 0; j < height; i++) 
+            {
+                int sumLine = 0;
+                for (int j = 0; j < height; ++j)
+                {
+                if (board[i,j] == Sign)
+                        ++sumLine;
+                }
+            if (sumLine == height)
+                    return true;
+            }
+
+        // Check crosses
+
+        int sumCrossA = 0;
+        int sumCrossB = 0;
+        for (int k = 0; k < width; ++k)
         {
+            if (board[k,k] == Sign)
+                ++sumCrossA;
+            if (board[k, width - 1 - k] == Sign)
+                ++sumCrossB;
+        }
+        if (sumCrossA == width || sumCrossB == width)
+                return true;
+
+
+        // If doesnt match any
         return false;
         }
-        }
 
-       class ComputerPlayer : Player, IMove 
+    class HumanPlayer : Player, IMove 
+    {
+        public bool MakeMove(char[,] board, char[,] boardCopy)
         {
-            public bool MakeMove(char[,] board)
-            {
-            return false;
-            }
+            return CheckIfGameEnded(board);
         }
+    }
+
+    class ComputerPlayer : Player, IMove 
+    {
+        public bool MakeMove(char[,] board, char[,] boardCopy)
+        {
+            return CheckIfGameEnded(board);
+        }
+    }
 }
